a6ab3af987b25781b049d18682039b14af5a34a4
Raphael <raphael.sulzer1@gmail.com>
2019-09-12 12:18:18 +0200

    after make install (inside build directory) this version every works with surfaceReconstruction. this is because this version gets then installed to usr/local/ where all the necessary dependencies can be found.

---
+++ build/lib/VLFeat/CMakeFiles/vlfeat.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/aib.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/aib.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/array.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/array.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/covdet.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/covdet.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/dsift.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/dsift.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/fisher.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/fisher.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/generic.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/generic.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/getopt_long.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/getopt_long.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/gmm.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/gmm.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/hikmeans.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/hikmeans.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/hog.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/hog.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/homkermap.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/homkermap.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/host.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/host.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/ikmeans.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/ikmeans.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/imopv.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/imopv.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/imopv_sse2.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/imopv_sse2.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/kdtree.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/kdtree.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/kmeans.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/kmeans.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/lbp.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/lbp.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/liop.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/liop.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/mathop.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/mathop.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/mathop_avx.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/mathop_avx.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/mathop_sse2.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/mathop_sse2.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/mser.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/mser.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/pgm.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/pgm.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/quickshift.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/quickshift.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/random.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/random.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/rodrigues.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/rodrigues.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/scalespace.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/scalespace.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/sift.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/sift.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/slic.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/slic.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/stringop.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/stringop.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/svm.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/svm.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/svmdataset.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/svmdataset.c.o"
+  "/home/raphael/PhD_local/cpp/colmap/lib/VLFeat/vlad.c" "/home/raphael/PhD_local/cpp/colmap/build/lib/VLFeat/CMakeFiles/vlfeat.dir/vlad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CGAL_ENABLED"
+  "CUDA_ENABLED"
+  "OPENGL_ENABLED"
+  "OPENMP_ENABLED"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib"
+  "../src"
+  "/usr/include/eigen3"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/local/include"
+  "/usr/include/x86_64-linux-gnu"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/colmap_autogen.dir/AutogenInfo.cmake
@@ -0,0 +1,39 @@
+# Meta
+set(AM_MULTI_CONFIG "SINGLE")
+# Directories and files
+set(AM_CMAKE_BINARY_DIR "/home/raphael/PhD_local/cpp/colmap/build/")
+set(AM_CMAKE_SOURCE_DIR "/home/raphael/PhD_local/cpp/colmap/")
+set(AM_CMAKE_CURRENT_SOURCE_DIR "/home/raphael/PhD_local/cpp/colmap/src/")
+set(AM_CMAKE_CURRENT_BINARY_DIR "/home/raphael/PhD_local/cpp/colmap/build/src/")
+set(AM_CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE "")
+set(AM_BUILD_DIR "/home/raphael/PhD_local/cpp/colmap/build/src/colmap_autogen")
+set(AM_SOURCES "")
+set(AM_HEADERS "")
+# Qt environment
+set(AM_QT_VERSION_MAJOR "5")
+set(AM_QT_VERSION_MINOR "9")
+set(AM_QT_MOC_EXECUTABLE )
+set(AM_QT_UIC_EXECUTABLE )
+set(AM_QT_RCC_EXECUTABLE "/usr/lib/qt5/bin/rcc")
+# MOC settings
+set(AM_MOC_SKIP )
+set(AM_MOC_DEFINITIONS )
+set(AM_MOC_INCLUDES )
+set(AM_MOC_OPTIONS )
+set(AM_MOC_RELAXED_MODE )
+set(AM_MOC_MACRO_NAMES )
+set(AM_MOC_DEPEND_FILTERS )
+set(AM_MOC_PREDEFS_CMD )
+# UIC settings
+set(AM_UIC_SKIP )
+set(AM_UIC_TARGET_OPTIONS )
+set(AM_UIC_OPTIONS_FILES )
+set(AM_UIC_OPTIONS_OPTIONS )
+set(AM_UIC_SEARCH_PATHS )
+# RCC settings
+set(AM_RCC_SOURCES "/home/raphael/PhD_local/cpp/colmap/src/ui/resources.qrc")
+set(AM_RCC_BUILDS "/home/raphael/PhD_local/cpp/colmap/build/src/colmap_autogen/UYX5XTB5RZ/qrc_resources.cpp")
+set(AM_RCC_OPTIONS "{-name;resources}")
+set(AM_RCC_INPUTS "{/home/raphael/PhD_local/cpp/colmap/src/ui/shaders/lines.v.glsl;/home/raphael/PhD_local/cpp/colmap/src/ui/shaders/points.f.glsl;/home/raphael/PhD_local/cpp/colmap/src/ui/shaders/triangles.f.glsl;/home/raphael/PhD_local/cpp/colmap/src/ui/shaders/lines.g.glsl;/home/raphael/PhD_local/cpp/colmap/src/ui/shaders/points.v.glsl;/home/raphael/PhD_local/cpp/colmap/src/ui/shaders/triangles.v.glsl;/home/raphael/PhD_local/cpp/colmap/src/ui/shaders/lines.f.glsl;/home/raphael/PhD_local/cpp/colmap/src/ui/media/import-from.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/reconstruction-start.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/reconstruction-options.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/project-open.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/export-all.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/feature-matching.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/render-disabled.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/reconstruction-reset.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/export-as-text.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/undistort.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/project-save-as.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/reconstruction-step.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/automatic-reconstruction.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/render-reset-view.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/render-options.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/reconstruction-pause.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/match-matrix.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/reconstruction-stats.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/bundle-adjustment.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/dense-reconstruction.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/database-management.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/render-enabled.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/project-new.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/import.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/project-edit.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/project-save.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/grab-movie.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/reconstruction-normalize.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/log.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/export.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/feature-extraction.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/grab-image.png;/home/raphael/PhD_local/cpp/colmap/src/ui/media/export-as.png}")
+# Configurations options
+set(AM_CONFIG_SUFFIX_Release "_Release")
